bd047f5097c93efb6ac64275ec62924a6097fb20
Glo <glo.movil@gmail.com>
2022-12-18 21:52:29 +0100

    sp3e4 retoque boton y reset cart

--- js/shop.js
+++ js/shop.js
@@ -119,8 +119,10 @@ function calculateTotal() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    cart.length = 0;
     
+    //cleaning cart in case we update it after pressing the button
+    cart.length = 0;
+        
     for (let i = 0; i < cartList.length; i++) {
         
         //if cart is empty, add first item
